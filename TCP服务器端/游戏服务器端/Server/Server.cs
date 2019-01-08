fef7b73bfac3e72e513f011e3e81f97af53cd624
Oceantears <44494020+Oceantears@users.noreply.github.com>
2019-01-08 18:40:38 +0800

    1

---
+++ TCP服务器端/游戏服务器端/Server/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace GameServer.Server
+{
+    class Server
+    {
+        private IPEndPoint ipEndPoint;
+        private Socket serverSocket;
+
+        public Server(){      }
+
+        public Server(string ipStr, int port)
+        {
+            SetIpAndPort(ipStr,port);
+        }
+
+        public void SetIpAndPort(string ipStr, int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ipStr), port);
+        }
+
+        public void Start()
+        {
+            serverSocket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+        }
+    }
+}
